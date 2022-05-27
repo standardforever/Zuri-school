1c0a77750c8c5afe61df1317338ee0b3ed107dd9
standardforever <standard.forever123@gmail.com>
2022-05-27 19:02:11 +0100

    updated

--- PYTHON_FILES/Classes-and-Objects/main.py
+++ PYTHON_FILES/Classes-and-Objects/main.py
@@ -6,12 +6,15 @@ class Student:
         self.tracks = kward["tracks"]
         self.score = kward["score"]
 
+    def __repr__(self):
+        return (f'Student(name={self.name!r}, 'f'age={self.age!r}, ' f'tracks={self.tracks!r}, ' f'score={self.score})')
+
     def change_name(self, new_name):
         self.name = new_name
     def change_age(self, new_age):
         self.age = new_age
     def add_track(self, new_track):
-            self.name = new_track
+            self.tracks= new_track
     def get_score(self):
         return self.score
 
@@ -20,6 +23,7 @@ Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 print(Bob)
 # Expected methods
 Bob.change_name("Peter")
+print(Bob)
 Bob.change_age(34)
 Bob.add_track("UI/UX")
 print(Bob.get_score())
